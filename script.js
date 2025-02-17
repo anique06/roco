cc94c8362eabe1d4606812a8fda66b13ed39410d
anique06 <aniqueikhwan06@gmail.com>
2025-02-18 01:36:35 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+function showTimeInputs() {
+    let routine = document.getElementById("routine").value;
+    let timeInputs = document.querySelectorAll(".time-field");
+
+    // Hide all time fields first
+    timeInputs.forEach((input) => {
+        input.style.display = "none";
+        input.removeAttribute("required");
+    });
+
+    // Show the required number of time fields
+    for (let i = 0; i < routine; i++) {
+        timeInputs[i].style.display = "block";
+        timeInputs[i].setAttribute("required", "true");
+    }
+}
+
+function submitData() {
+    let routine = document.getElementById("routine").value;
+    if (!routine) {
+        alert("Please select how many times you take medicine per day.");
+        return;
+    }
+
+    let time1 = document.getElementById("time1").value || "00:00";
+    let time2 = document.getElementById("time2").value || "00:00";
+    let time3 = document.getElementById("time3").value || "00:00";
+
+    alert("Your data has been saved successfully!");
+
+    // Send data to ESP32
+    let esp32_url = `http://192.168.4.1/save?routine=${routine}&time1=${time1}&time2=${time2}&time3=${time3}`;
+    fetch(esp32_url)
+        .then(response => console.log("Data sent to ESP32"))
+        .catch(error => console.error("Error:", error));
+}
